9863613e511249b79b4fdbb52935bc15c2dff6a3
Marcus <107251339+marcusprodev@users.noreply.github.com>
2022-12-16 03:05:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function calcular(){
+    var carro = parseInt(document.getElementById("carro").value);
+    var imposto = 45/100;
+    var distribuidor = 28/100;
+    calculo.value =(carro + imposto + distribuidor);   
+}
